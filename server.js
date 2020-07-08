2fe12f5c5ec0263172728bd7b48a9b0b57cd5353
Phuong Le <phuonganh.le0312@gmail.com>
2020-07-08 19:43:58 -0400

    fixed app.get and app.post

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 // create an instance of express
 const app = express();
 // add a port
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.PORT || 3000;
 
 // sets up the express app to handle data parsing
 app.use(express.urlencoded({extended: true}));
@@ -19,65 +19,72 @@ app.use(express.static(path.join(__dirname, "Develop/public")));
 // GET requests
 // View / HTML
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "Develop/public/assets/index.html"));
+    res.sendFile(path.join(__dirname, "Develop/public/index.html"));
 });
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "Develop/public/assets/notes.html"));
+    res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
 });
 
 // API/JSON
 // retrieve note data
 
-// app.get("/api/notes", function (req, res) {
-//     res.sendFile(path.join(__dirname + "/public/db/db.json"));
-// });
-let notesData = [];
-module.exports = function (app) {
-app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "Develop/db/db.json"));
-    res.JSON(notesData);
-});
-};
-// post note data
-app.post("/api/notes", function (req, res) {
-    //read and parse data from json
-    notesData = fs.readFileSync("Develop/db/db.json", "utf8");
-    notesData= JSON.parse(notesData);
-    let note = req.body;
-    note.id = notesData.length + 1;
-    notesData.push(note);
-    notesData = JSON.stringify(notesData);//might be problem
 
-    fs.writeFile("Develop/db/db.json", JSON.stringify(notesData), "utf-8", (err) => {
-        if (err) 
-            throw err;
-        
-        console.log("Your note has been posted.");
-    
-    res.json(JSON.parse(notesData));
- 
+// module.exports = function (app) {
+app.get("/api/notes", function (req, res) {
+    fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
+        if (err) {
+            console.log("An error has occured reading your data.")
+        }
+    const notesData = JSON.parse(data);
+    res.json(notesData);
+    });
 });
+
+// post note data
+app.post("/api/notes", function (req, res) { // read and parse data from json
+    fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
+        if (err) {
+            console.log("An error has occured reading your data.")
+        }
+        const notesData = JSON.parse(data);
+        let note = req.body;
+        note.id = notesData.length + 1;
+        fs.writeFile("Develop/db/db.json", JSON.stringify(notesData), "utf-8", (err) => {
+            if (err) 
+                throw err;
+            
+
+            console.log("Your note has been posted.");
+
+            res.json(notesData);
+        });
+    });
     // delete note
-    app.delete("/api/notes/:id", function(req, res) {
-        notesData= fs.readFileSync("Develop/db/db.json", "utf-8");
-        notesData = JSON.parse(notesData);
-        consttoDelete = notesData.filter((note) => note.id != parseInt(req.params.id));
+    app.delete("/api/notes/:id", function (req, res) {
+        fs.readFile("Develop/db/db.json", "utf-8", (err, data) => {
+            if (err) {
+                console.log("An error has occured reading your data.")
+            };
+            const notesData = JSON.parse(data);
+            const toDelete = notesData.filter((note) => note.id != parseInt(req.params.id));
             fs.writeFile("Develop/db/db.json", JSON.stringify(toDelete), "utf-8", (err) => {
                 if (err) 
                     throw err;
-    
-                    console.log("Your note has been deleted.");
+                
+
+                console.log("Your note has been deleted.");
+
             });
             res.json(toDelete);
-          });
-        
-        
+        });
+    });
 
-   
-    })
+    
+    });
 
 
-// listen on port
-app.listen(PORT, (req, res) => {
-    console.log(`Currently running on http://localhost:${PORT}`);
-});
+    // listen on port
+    app.listen(PORT, (req, res) => {
+        console.log(`Currently running on http://localhost:${PORT}`);
+    });
+
