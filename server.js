f9d9133cb804b37a8c53c225a94b795e2b25e9b2
Phuong Le <phuonganh.le0312@gmail.com>
2020-07-08 22:56:11 -0400

    cleaned up code

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "Develop/public")));
-
+// data array
+const notesData = [];
 
 // GET requests
 // View / HTML
@@ -38,51 +39,36 @@ app.get("/api/notes", function (req, res) {
 });
 
 // post note data
-app.post("/api/notes", function (req, res) { // read and parse data from json
-    fs.readFile("Develop/db/db.json", "utf-8", (err, data) => {
+app.post("/api/notes", function (req, res) {
+    const note = {
+        ... req.body,
+        id: notesData.length
+    };
+    notesData.push(note);
+
+    fs.writeFile("Develop/db/db.json", JSON.stringify(notesData), "utf-8", (err) => {
         if (err) {
-            console.log("An error has occured reading your data.")
-        };
-        const notesData = [];
-        notesData.push(JSON.parse(data));
-        const note = {
-            ... req.body,
-            id: notesData.length
-        };
-        notesData.push(note);
+            throw(err);
+        }
+        console.log("Your note has been posted.");
 
-        fs.writeFile("Develop/db/db.json", JSON.stringify(notesData), "utf-8", (err) => {
-            if (err) 
-                throw err;
-            
-
-
-            console.log("Your note has been posted.");
-
-            res.json(notesData);
-        });
+        res.json(notesData);
     });
 });
+
 // delete note
 app.delete("/api/notes/:id", function (req, res) {
-    fs.readFile("Develop/db/db.json", "utf-8", (err, data) => {
+    const toDelete = notesData.filter((note) => note.id != parseInt(req.params.id));
+    fs.writeFile("Develop/db/db.json", JSON.stringify(toDelete), "utf-8", (err) => {
         if (err) {
-            console.log("An error has occured reading your data.")
-        };
-        const notesData = JSON.parse(data);
-        const toDelete = notesData.filter((note) => note.id != parseInt(req.params.id));
-        fs.writeFile("Develop/db/db.json", JSON.stringify(toDelete), "utf-8", (err) => {
-            if (err) 
-                throw err;
-            
-
-
-            console.log("Your note has been deleted.");
-
-        });
-        res.json(toDelete);
+            throw (err);
+        }
+        console.log("Your note has been deleted.");
     });
+    res.json(toDelete);
 });
+
+
 // listen on port
 app.listen(PORT, (req, res) => {
     console.log(`Currently running on http://localhost:${PORT}`);
